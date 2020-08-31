954a96eb392bc0b5f91cb401a253e93ed0af5ae9
florin-avram <avram.florin.c@gmail.com>
2020-08-31 18:18:57 +0200

    Add React Router components for navigation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,31 +4,35 @@ import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props)  {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
 
     render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} 
-                      onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail 
-                    dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes }/>} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
-    );
-  }
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
+        );
     }
 }
 
