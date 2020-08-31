964a0660002ea432516171b92252bf6393d3400c
florin-avram <avram.florin.c@gmail.com>
2020-08-31 17:33:29 +0200

    Add Header and Footer components to the React App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Footer from './FooterComponent';
+import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -17,17 +18,12 @@ class Main extends Component {
     render() {
         return (
             <div>
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">
-                        Ristorante Con Fusion
-                    </NavbarBrand>
-                </div>
-            </Navbar>
-            <Menu dishes={this.state.dishes} 
-                  onClick={(dishId) => this.onDishSelect(dishId)} />
-            <DishDetail 
-                dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Header />
+                <Menu dishes={this.state.dishes} 
+                      onClick={(dishId) => this.onDishSelect(dishId)} />
+                <DishDetail 
+                    dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
             </div>
     );
   }
