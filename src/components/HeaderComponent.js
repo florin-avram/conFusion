ac2f8ddd4faa424a3f46f0b3f9530ad223e32662
florin-avram <avram.florin.c@gmail.com>
2020-09-02 14:34:17 +0200

    Assignment 2. Add about us component to the Single Page Application

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -18,7 +18,7 @@ class Header extends Component {
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src="public/assets/images/logo.png"
+                            <img src="/assets/images/logo.png"
                                  height="30"
                                  width="41"
                                  alt="Ristorante Con Fusion" />
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -51,6 +52,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes }/>} />
                     <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
